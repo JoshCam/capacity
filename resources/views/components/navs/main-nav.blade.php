83c8b024ae54fa3c73356429495753389217f9e1
Josh Campden <joshcampden15@gmail.com>
2021-11-11 11:43:33 +0000

    fixed home btn

--- resources/views/components/navs/main-nav.blade.php
+++ resources/views/components/navs/main-nav.blade.php
@@ -50,11 +50,13 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="/home"
-                        >Home</a
-                    >
-                </li>
+                @auth
+                    <li class="nav-item">
+                        <a class="nav-link" href="/home"
+                            >Home</a>
+                    </li>
+                @endauth
+
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('clubs.nearBy') }}">Near By</a>
                 </li> -->
