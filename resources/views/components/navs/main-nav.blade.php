e007bf83aba9747d02744ca80767022fc5ae5ad0
Josh Campden <joshcampden15@gmail.com>
2021-11-11 09:09:01 +0000

    cleaned up nav bar again

--- resources/views/components/navs/main-nav.blade.php
+++ resources/views/components/navs/main-nav.blade.php
@@ -50,11 +50,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+                <!-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('clubs.index') }}"
                         >Home</a
                     >
-                </li>
+                </li> -->
                 <!-- <li class="nav-item">
                     <a class="nav-link" href="{{ route('clubs.nearBy') }}">Near By</a>
                 </li> -->
