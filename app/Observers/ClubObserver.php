47ddac6c352ec569a6e07bd7c0e38919c0aeec59
Josh Campden <joshcampden15@gmail.com>
2022-01-18 11:52:49 +0000

    added event on occupancy @ 80%

---
+++ app/Observers/ClubObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Jobs\NotifyUsersOfCapacity;
+use App\Models\Club;
+
+class ClubObserver
+{
+    /**
+     * Handle the Club "created" event.
+     *
+     * @param  \App\Models\Club  $club
+     * @return void
+     */
+    public function created(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Handle the Club "updated" event.
+     *
+     * @param  \App\Models\Club  $club
+     * @return void
+     */
+    public function updated(Club $club)
+    {
+        // if ($club->occupancy == $club->capacity * 0.8) {
+            NotifyUsersOfCapacity::dispatch($club);
+        // }
+    }
+
+    /**
+     * Handle the Club "deleted" event.
+     *
+     * @param  \App\Models\Club  $club
+     * @return void
+     */
+    public function deleted(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Handle the Club "restored" event.
+     *
+     * @param  \App\Models\Club  $club
+     * @return void
+     */
+    public function restored(Club $club)
+    {
+        //
+    }
+
+    /**
+     * Handle the Club "force deleted" event.
+     *
+     * @param  \App\Models\Club  $club
+     * @return void
+     */
+    public function forceDeleted(Club $club)
+    {
+        //
+    }
+}
