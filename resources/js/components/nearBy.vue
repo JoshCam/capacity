98895732c08d99404e57539f0499fdf684a5bd64
Josh Campden <joshcampden15@gmail.com>
2022-04-08 16:41:38 +0100

    Rubbish error handling on nearby page

--- resources/js/components/nearBy.vue
+++ resources/js/components/nearBy.vue
@@ -1,11 +1,16 @@
 <template>
-    <div class="row">
-        <club-card
-            v-for="club in sortedClubs"
-            :key="club.id"
-            :club="club"
-            class="col-md-4 mb-3"
-        />
+    <div>
+        <div class="row" v-if="sortedClubs.length > 0">
+            <club-card
+                v-for="club in sortedClubs"
+                :key="club.id"
+                :club="club"
+                class="col-md-4 mb-3"
+            />
+        </div>
+        <div class="row" v-else>
+            <h1>It doesn't look like you're near any clubs :'(</h1>
+        </div>
     </div>
 </template>
 
@@ -35,7 +40,6 @@ export default {
     watch: {
         userCoords: function () {
             this.getClubsInRadius(this.userCoords);
-            this.sortClubs(this.clubs);
         },
     },
 
@@ -52,6 +56,7 @@ export default {
         getClubsInRadius(userCoords) {
             let clubs = [];
             window.axios.post("/api/radius/", userCoords).then((response) => {
+                console.log(response.data.length);
                 clubs = response.data;
                 clubs.sort((a, b) => {
                     return a.distance - b.distance;
